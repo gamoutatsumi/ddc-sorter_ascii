47141abbe380e55acf6f9b7d65f7924ce8ba172e
gamoutatsumi <tatsumi@gamou.dev>
2023-11-08 15:53:44 +0900

    update dependencies

--- denops/@ddc-filters/sorter_ascii.ts
+++ denops/@ddc-filters/sorter_ascii.ts
@@ -1,8 +1,5 @@
-import {
-  BaseFilter,
-  Item,
-} from "https://deno.land/x/ddc_vim@v3.9.0/types.ts";
-import { Denops } from "https://deno.land/x/ddc_vim@v3.9.0/deps.ts";
+import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
 
 function calcStore(a: Item, b: Item): number {
   const wordA = a.word.toLowerCase();
